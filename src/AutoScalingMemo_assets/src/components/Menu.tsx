9a3bd8af7a5f7ef75a7eb8b331fe2aef9d109b53
hoosan <hoosanalis@gmail.com>
2022-04-09 18:28:51 +0900

    Added a login menu to the side bar

--- src/AutoScalingMemo_assets/src/components/Menu.tsx
+++ src/AutoScalingMemo_assets/src/components/Menu.tsx
@@ -19,19 +19,14 @@ import menuIcon from '@iconify/icons-bx/menu';
 import addPage from '@iconify/icons-iconoir/add-page';
 import bookStack from '@iconify/icons-iconoir/book-stack';
 import logoutCircleRLine from '@iconify/icons-ri/logout-circle-r-line';
+import loginCircleLine from '@iconify/icons-ri/login-circle-line';
 
 import { useAuthentication } from '../hooks/useAuthentication';
 
 export const Menu = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { handleLogoutClick } = useAuthentication();
+  const { handleLogoutClick, handleLoginClick, isLogin } = useAuthentication();
   const navigate = useNavigate();
-
-  const handleClick = async () => {
-    await handleLogoutClick();
-    navigate('/');
-    onClose();
-  };
 
   return (
     <>
@@ -44,28 +39,52 @@ export const Menu = () => {
           <DrawerHeader borderBottomWidth='1px'>Auto Scaling Memo</DrawerHeader>
           <DrawerBody fontSize='lg'>
             <List alignItems='left' spacing='20px'>
-              <ListItem>
-                <Link to='/new'>
-                  <HStack mt='10px'>
-                    <InlineIconIconify icon={addPage} height='24' />
-                    <Text>New Memo</Text>
+              {isLogin ? (
+                <>
+                  <ListItem>
+                    <Link to='/new'>
+                      <HStack mt='10px'>
+                        <InlineIconIconify icon={addPage} height='24' />
+                        <Text>New Memo</Text>
+                      </HStack>
+                    </Link>
+                  </ListItem>
+                  <ListItem>
+                    <Link to='/'>
+                      <HStack>
+                        <InlineIconIconify icon={bookStack} height='24' />
+                        <Text>Your Memos</Text>
+                      </HStack>
+                    </Link>
+                  </ListItem>
+                  <ListItem>
+                    <HStack
+                      as='button'
+                      onClick={async () => {
+                        await handleLogoutClick();
+                        navigate('/');
+                        onClose();
+                      }}
+                    >
+                      <InlineIconIconify icon={logoutCircleRLine} height='24' />
+                      <Text>Logout</Text>
+                    </HStack>
+                  </ListItem>
+                </>
+              ) : (
+                <ListItem>
+                  <HStack
+                    as='button'
+                    onClick={async () => {
+                      await handleLoginClick();
+                      onClose();
+                    }}
+                  >
+                    <InlineIconIconify icon={loginCircleLine} height='24' />
+                    <Text>Login</Text>
                   </HStack>
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link to='/'>
-                  <HStack>
-                    <InlineIconIconify icon={bookStack} height='24' />
-                    <Text>Your Memos</Text>
-                  </HStack>
-                </Link>
-              </ListItem>
-              <ListItem>
-                <HStack as='button' onClick={handleClick}>
-                  <InlineIconIconify icon={logoutCircleRLine} height='24' />
-                  <Text>Logout</Text>
-                </HStack>
-              </ListItem>
+                </ListItem>
+              )}
             </List>
           </DrawerBody>
         </DrawerContent>
