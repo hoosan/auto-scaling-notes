bcec58591b86816dab4f59677a7763fd1b1997ec
hoosan <hoosanalis@gmail.com>
2022-04-10 20:51:55 +0900

    Added a workaround for a bug in moc

--- install_local.sh
+++ install_local.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# For the testing purpose, a data size is set to 1 GB.
-# Use a smaller number for productions.
 dfx canister create Datastore
 dfx build Datastore
-dfx deploy --with-cycles 80000000000000 AutoScalingNote
-dfx deploy AutoScalingNote_assets
-# dfx canister call AutoScalingNote initDataStoreCanister '()'
+echo "yes" | dfx deploy --with-cycles 80000000000000 AutoScalingNote
+echo "yes" | dfx deploy AutoScalingNote_assets
